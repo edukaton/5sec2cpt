90396538cfc73f9f18eba160a21a0a0191e3c259
Jan Horubała <jan.horubala@gmail.com>
2018-02-17 18:41:31 +0100

    added routes: quest, result

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,64 +5,22 @@
   <div>
     <div class="ui top attached menu">
       <div class="ui container">
-
         <div class="item">
           <h3>
             Zagrożenie #{{ index + 1 }}
           </h3>
         </div>
         <div class="right menu">
-         <sui-progress
-           state="active"
-           indicating
-           :percent="percent"
-         />
-       </div>
-     </div>
-   </div>
-   <div class="ui grid container">
-    <div class="row">
-      <div class="column ten wide centered">
-        <img class="ui image left floated" :src="current.img">
-        <h3>
-          {{ current.title }}
-        </h3>
-        <p>
-          {{ current.text }}
-        </p>
-      </div>
-    </div>
-    <!-- <div class="ui divider"></div> -->
-    <div class="row centered">
-      <h1>Wybierz 2 Umiejętności Obronne</h1>
-    </div>
-    <div class="ui row ui four doubling cards">
-      <div v-for="s, i in user_skills" class="ui card fluid link" @click="select(i)">
-        <div class="content">
-          <div class="header">
-            {{ s.title }}
-          </div>
-        </div>
-        <div class="image">
-          <img :src="s.img">
-        </div>
-        <div class="content">
-          <div class="description">
-            {{ s.text }}
-          </div>
-        </div>
-        <div v-if="!choose(i)" class="ui bottom attached button">
-          <i class="add icon"></i>
-          Wybierz Kartę
-        </div>
-        <div v-else class="ui bottom green attached button">
-          <i class="checkmark icon"></i>
-          Wybrano!
+          <sui-progress
+          state="active"
+          indicating
+          :percent="percent"
+          ></sui-progress>
         </div>
       </div>
     </div>
+    <router-view></router-view>
   </div>
-</div>
 </template>
 
 <script>
@@ -71,18 +29,6 @@ import { mapGetters } from 'vuex'
 export default {
   computed: mapGetters([
     'index',
-    'chosen',
-    'percent',
-    'current',
-    'user_skills'
-    ]),
-  methods: {
-    select(index) {
-      this.$store.commit('chose', index)
-    },
-    choose(index) {
-      return this.chosen(index)
-    },
-  }
+  ]),
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Game from '@/components/Game'
+import Quest from '@/components/Quest'
+import Result from '@/components/Result'
 import LandingPage from '@/components/LandingPage'
 import NewScenario from '@/components/NewScenario'
 import ChooseHero from '@/components/ChooseHero'
@@ -9,23 +11,33 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/gra',
-      name: 'game',
-      component: Game,
-    },
-    {
-      path: '/',
-      name: 'LandingPage',
-      component: LandingPage
-    }, {
-      path: '/nowy-scenariusz',
-      name: 'NewScenario',
-      component: NewScenario
-    }, {
-      path: '/wybor-postaci',
-      name: 'ChooseHero',
-      component: ChooseHero
-    }
-  ]
+  {
+    path: '/gra',
+    name: 'game',
+    component: Game,
+    children: [
+      {
+        path: 'zagrozenie',
+        component: Quest
+      },
+      {
+        path: 'rozwiazanie',
+        component: Result
+      },
+    ],
+},
+{
+  path: '/',
+  name: 'LandingPage',
+  component: LandingPage
+}, {
+  path: '/nowy-scenariusz',
+  name: 'NewScenario',
+  component: NewScenario
+}, {
+  path: '/wybor-postaci',
+  name: 'ChooseHero',
+  component: ChooseHero
+}
+]
 })
