300e38bb7d8b08af4a7c0bd9af9f2d40a752def0
Jan Horubała <jan.horubala@gmail.com>
2018-02-17 18:33:25 +0100

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-import HelloWorld from '@/components/HelloWorld'
 import Game from '@/components/Game'
-=======
 import LandingPage from '@/components/LandingPage'
 import NewScenario from '@/components/NewScenario'
 import ChooseHero from '@/components/ChooseHero'
->>>>>>> 4c7df14b7a18b7426f6fc7e7ae4d30497c4d2696
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-<<<<<<< HEAD
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/gra',
       name: 'game',
       component: Game,
-=======
+    },
+    {
+      path: '/',
       name: 'LandingPage',
       component: LandingPage
     }, {
@@ -34,7 +26,6 @@ export default new Router({
       path: '/wybor-postaci',
       name: 'ChooseHero',
       component: ChooseHero
->>>>>>> 4c7df14b7a18b7426f6fc7e7ae4d30497c4d2696
     }
   ]
 })
