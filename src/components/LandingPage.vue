8b8f23ac2e98f8cd627b056d048b175672ffefc9
Katarzyna Wrzesinska <katarzyna.wrzesinska@tedd.berlin>
2018-02-18 12:42:33 +0100

    new icons on landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,20 +2,25 @@
   <div class="hello ui two column centered grid">
     <div class="column aligned">
       <h1>Internet - czas się bronić!</h1>
-      <img src="./../assets/landing_img.png">
+      <div class="ui grid icons">
+        <img class="icon" src="./../assets/32-office-icons_2.svg">
+        <img class="icon" src="./../assets/32-office-icons_5.svg">
+        <img class="icon" src="./../assets/32-office-icons_11.svg">
+      </div>
+
       <ul>
         <li>
-          <router-link to="/wybor-postaci" class="ui primary button">
-            Graj
+          <router-link to="/wybor-postaci" class="ui primary button huge">
+            Rozpocznij grę
           </router-link>
-          <router-link to="/nowy-scenariusz" class="ui primary button">
-            Dodaj
+          <router-link to="/nowy-scenariusz" class="ui primary basic button huge">
+            Nowe zagrożenie
           </router-link>
         </li>
       </ul>
-      <h5>Czy wiesz, że każdego dnia zostawiasz w internecie ślad po swojej aktywności? To mogą być Twoje dane osobowe, ale także gust do ubrań, upodobania żywieniowe lub prywatne zdjęcia. Pozostawione przez Ciebie dane mogą zostać użyte przez osoby, które chcą Ci zaszkodzić. Otaczająca Cię bańka informacyjna może ograniczyć Ci dostęp do różnorodnej wiedzy. Nie daj się! Naucz się bezpiecznie korzystać z internetu!
+      <p class="info">Czy wiesz, że każdego dnia zostawiasz w internecie ślad po swojej aktywności? To mogą być Twoje dane osobowe, ale także gust do ubrań, upodobania żywieniowe lub prywatne zdjęcia. Pozostawione przez Ciebie dane mogą zostać użyte przez osoby, które chcą Ci zaszkodzić. Otaczająca Cię bańka informacyjna może ograniczyć Ci dostęp do różnorodnej wiedzy. Nie daj się! Naucz się bezpiecznie korzystać z internetu!
 
-      </h5>
+      </p>
     </div>
 
   </div>
@@ -34,8 +39,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+h1 {
+  font-size: 45px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.icon {
+  max-height: 160px;
+}
+.icons {
+  margin-bottom: 50px;
+  margin-top: 50px;
+}
+p.info {
+  margin-top: 30px;
+  font-size: 1.1em;
 }
 ul {
   list-style-type: none;
